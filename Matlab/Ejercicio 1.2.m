f015e3900f35e71e16440dda6bd565f6902c2b89
Armando_Rdh <Armando_Rdh@hotmail.com>
2023-02-09 23:03:08 -0800

    Tema 1.2 completo, 1.3 empezado

--- Matlab/Ejercicio 1.2.m
+++ Matlab/Ejercicio 1.2.m
@@ -1,11 +1,8 @@
 n = [0:20]
 w0 = 0
-#x = (cos(0*n))
+x = (cos(0*n))
 #x = (cos((pi/8)*n))
-x = (cos((pi/4)*n))
-x4 = (cos((pi/2)*n))
+#x = (cos((pi/4)*n))
+#x = (cos((pi/2)*n))
 #x = (cos((pi)*n))
-subplot(
 stem(n,x)
-
-
